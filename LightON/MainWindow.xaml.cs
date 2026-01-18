a3ff670febae6e9f64a0247ddf420ecedae9cb5f
GLYSATVIK <codeforcessatvik@gmail.com>
2026-01-18 16:45:10 +0530

    feat(overlay): add Win32 interop for transparent click-through screen overlay

---
+++ LightON/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LightON_Final.Services;
+using System;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace LightON_Final
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            // Make window size match the primary screen
+            this.Width = SystemParameters.PrimaryScreenWidth;
+            this.Height = SystemParameters.PrimaryScreenHeight;
+            
+            // Allow dragging the window by clicking anywhere on it
+            this.MouseLeftButtonDown += (s, e) => this.DragMove();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            // Enable click-through so users can click through the ring
+            WindowServices.SetClickThrough(this);
+        }
+    }
+}
---
+++ LightON/Services/WindowServices.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace LightON_Final.Services
+{
+    public static class WindowServices
+    {
+        private const int WS_EX_TRANSPARENT = 0x00000020;
+        private const int WS_EX_TOOLWINDOW = 0x00000080;
+        private const int GWL_EXSTYLE = -20;
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+        public static void SetClickThrough(Window window)
+        {
+            var hwnd = new WindowInteropHelper(window).Handle;
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW);
+        }
+
+        // Hotkey Constants
+        public const int WM_HOTKEY = 0x0312;
+        public const uint MOD_NONE = 0x0000;
+        public const uint MOD_ALT = 0x0001;
+        public const uint MOD_CONTROL = 0x0002;
+        public const uint MOD_SHIFT = 0x0004;
+        public const uint VK_L = 0x4C;
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+    }
+}
